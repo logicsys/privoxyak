c9a133c33e1b46ed97bae81c0d9d7bbfd3d31a6b
Nathan James <wobaekat@gmail.com>
2022-10-05 18:52:47 +0100

    berks bump for chef17

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'nathan.james@yakara.com'
 license          'Apache-2.0'
 description      'Installs and configures Privoxy'
 version          '0.1.0'
-chef_version     '>= 13.0'
+chef_version     '>= 17.0'
 
 source_url 'https://github.com/yakara-ltd/privoxyak'
 issues_url 'https://github.com/yakara-ltd/privoxyak/issues'
